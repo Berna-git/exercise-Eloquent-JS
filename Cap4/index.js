d980e30d6252088ddea154bf127bf56b01b57a5f
Bernabee Gonzalez <83044002+Berna-git@users.noreply.github.com>
2021-10-26 14:37:01 -0300

    Capitulo 4

--- Cap4/index.js
+++ Cap4/index.js
@@ -49,20 +49,28 @@ myArray = reverseArray(myArray);
 
 
 /// Ejercio 3!!! 
-let myList = [1,2,3];
-let list = {
-    value:{},
-    rest:{}
-};
-
-const createList = (arrayList) => {
-  for(let i = 0; arrayList.length > i; i++) {
-      list.value = arrayList[i];
-      list.rest = list  + arrayList[i + 1] ;
-      console.log(list);
-  }  
-  console.log(list);
-
-}
-
-createList(myList);
+function arrayToList(array) {
+    let list = null;
+    for (let i = array.length - 1; i >= 0; i--) {
+      list = {value: array[i], rest: list};
+    }
+    return list;
+  }
+  
+  function listToArray(list) {
+    let array = [];
+    for (let node = list; node; node = node.rest) {
+      array.push(node.value);
+    }
+    return array;
+  }
+  
+  function prepend(value, list) {
+    return {value, rest: list};
+  }
+  
+  function nth(list, n) {
+    if (!list) return undefined;
+    else if (n == 0) return list.value;
+    else return nth(list.rest, n - 1);
+  }
