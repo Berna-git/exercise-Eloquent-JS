8b977bea32e0ee7ebb4c9ce8a74db6c6bd656fbc
Bernabee Gonzalez <83044002+Berna-git@users.noreply.github.com>
2021-10-27 17:42:12 -0300

    ejercicios 5

---
+++ Cap5/index.js
@@ -0,0 +1,47 @@
+// Ejercico 1!!!
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+console.log(arrays)
+let newArray = arrays.reduce((previousValue, currentValue) => previousValue.concat(currentValue));
+console.log(newArray)   
+
+
+/// Exercise 3 
+
+// loop(3, (n => n > 0),( n => n - 1), console.log(n));
+
+//Inicializador
+//condicion
+//iterador
+// bloque de codigo
+
+function repetir(n, accion) {
+    for (let i = 0; i < n; i++) {
+      accion(i);
+    }
+}
+
+// repetir(3, console.log);
+
+// function condition(n) {
+//     if(n > 0) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+
+function iterator(n) {
+    if(n > 0) {
+        console.log('n es = ' + n)
+        n = n - 1; 
+        iterator(n); 
+    } else {
+        
+    }
+}
+
+
+function loop(n,iterador,staments) {
+       staments(iterador(n));
+}
+loop(5,iterator,console.log);
